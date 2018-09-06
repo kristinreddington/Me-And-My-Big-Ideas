d14ba61c1101455003a8a6df326cbe80b3829f67
kristinreddington <kristinreddington@gmail.com>
2018-09-05 23:13:44 -0400

    add form html

--- client/src/containers/IdeaForm.js
+++ client/src/containers/IdeaForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createIdea } from '../actions/IdeaActions'
 import { updateIdeaFormData } from '../actions/IdeaFormActions'
+import './Ideas.css'
 
 class IdeaForm extends Component {
   handleOnChange = (event) => {
@@ -20,23 +21,23 @@ handleOnSubmit = (event) => {
     const { title, description, images, notes } = this.props.ideaFormData;
     return(
       <div>
-      Add Product to inventory
-        <form onChange={this.handleOnChange} onSubmit={this.handleOnSubmit}>
+      Add Idea & get after it!
+        <form className="form-group" onChange={this.handleOnChange} onSubmit={this.handleOnSubmit}>
         <div>
           <label htmlFor="title">Title:</label>
-          <input type="text" name="title" value={title} />
+          <input className="form-control" type="text" name="title" value={title} />
         </div>
         <div>
-          <label htmlFor="description">Description:</label>
-          <input type="text" name="description" value={description} />
+          <label className="label" htmlFor="description">Description:</label>
+          <input className="form-control" type="textarea" name="description" value={description} />
         </div>
         <div>
-          <label htmlFor="images">Image Url:</label>
-          <input type="text" name="images" value={images} />
+          <label id="image-file" htmlFor="file">Image Url:</label>
+          <input id="image" className="form-control" type="file" accept="image/png, image/jpeg" name="file" value={images} />
         </div>
         <div>
-          <label htmlFor="notes">Notes:</label>
-          <input type="text" name="notes" value={notes} />
+          <label className="label" htmlFor="notes">Notes:</label>
+          <input className="form-control" type="textarea" name="notes" value={notes} />
         </div>
 
         <button type="submit">Add Idea</button>
